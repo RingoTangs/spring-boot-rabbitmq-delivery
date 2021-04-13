631007d34db3067974aff5a0ef08aa324df349e5
yummy <1466637477@qq.com>
2021-04-13 13:07:31 +0800

    生产端

--- order-producer/src/main/java/com/ymy/boot/producer/RabbitOrderSender.java
+++ order-producer/src/main/java/com/ymy/boot/producer/RabbitOrderSender.java
@@ -6,7 +6,6 @@ import com.ymy.boot.mapper.BrokerMessageLogMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -19,32 +18,40 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @Component
-public class RabbitOrderSender {
+public class RabbitOrderSender implements RabbitTemplate.ConfirmCallback {
     @Resource
     private RabbitTemplate rabbitTemplate;
 
     @Resource
     private BrokerMessageLogMapper brokerMessageLogMapper;
 
-    // 回调函数: confirm 确认
-    final RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
-        @Override
-        public void confirm(@Nullable CorrelationData correlationData, boolean ack, @Nullable String cause) {
-            log.info("correlationData：" + correlationData);
-            String messageId = correlationData.getId();
-            if (ack) {
-                // 如果confirm返回成功则进行更新
-                brokerMessageLogMapper.changeBrokerMessageLogStatus(PublisherConstants.ORDER_SENDING_SUCCESS, messageId);
-            } else {
-                // 失败则进行具体后续操作: 重试或者补偿等手段
-                System.out.println("重试或者补偿等手段...");
-            }
+    /**
+     * Broker消息返回确认
+     *
+     * @author Ringo
+     * @date 2021/4/12
+     */
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        log.info("correlationData：" + correlationData);
+        String messageId = correlationData.getId();
+        if (ack) {
+            // 如果confirm返回成功则进行更新
+            brokerMessageLogMapper.changeBrokerMessageLogStatus(PublisherConstants.ORDER_SENDING_SUCCESS, messageId);
+        } else {
+            // 失败则进行具体后续操作: 重试或者补偿等手段
+            log.info("投递消息失败 Broker Nack....");
         }
-    };
+    }
 
-    // 发送消息方法调用: 构建自定义对象消息
+    /**
+     * 向 Broker 发送消息调用该方法即可
+     *
+     * @author Ringo
+     * @date 2021/4/13
+     */
     public void sendOrder(Order order) {
-        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setConfirmCallback(this);
 
         // 消息唯一ID
         CorrelationData correlationData = new CorrelationData(order.getMessageId());
